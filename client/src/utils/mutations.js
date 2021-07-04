650fb196c6b13b739f74ee5af997940a0e27f97b
Tela Caul <telacaul@gmail.com>
2021-07-04 14:40:10 -0700

    mutations and queries files added to utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,67 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: bookDescription!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+          authors
+          description
+          bookId
+          image
+          link
+          title
+        }
+    }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook (bookId: $bookId) {
+        User {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+              }
+        }
+    }
+  }
+`;
