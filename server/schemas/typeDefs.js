5236a3a6618d34737112581dfa7dbdacec8154f7
Tela Caul <telacaul@gmail.com>
2021-07-04 19:45:09 -0700

    type defs with queries and mutations in server schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,52 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-    type Query {
-        helloWorld: String
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    createdAt: String
+    description: [Description]
+    title: String
+    image: String
+    link: String
+  }
+
+  type Description {
+    bookId: ID
+    authors: [String]
+    descriptionBody: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    savedBooks(username: String): [Book]
+    book(_id: ID!): Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookText: Description!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
